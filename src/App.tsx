7e83a92af5f7fe2352efec43bc60cd1e94c89fc6
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-02-21 22:03:46 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Root from "./routes/Root";
+import Home from "./routes/Root/Home";
+
 function App() {
 
   return (
-    <h1>Hello, world!</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} >
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+
+    return (
+        <header className="header">
+            <nav>
+                <p>Github API</p>
+            </nav>
+        </header>
+    );
+}
