7ce29910089f7f8d0c511b3e734aec726e2b7829
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-02-22 22:56:26 -0300

    Find page layout

--- src/routes/Root/Find/index.tsx
+++ src/routes/Root/Find/index.tsx
@@ -1,3 +1,4 @@
+import Button from '../../../components/Button';
 import './styles.css';
 
 export default function Find() {
@@ -6,7 +7,20 @@ export default function Find() {
         <main className="find-main">
             <section>
                 <div className="container find-container">
-                    <h1 className="find-title">Encontre um perfil Github</h1>
+                    <div className="find-title-container">
+                        <h1 className="find-title">Encontre um perfil Github</h1>
+                    </div>
+                    <div className="input-container">
+                        <input
+                            className="find-input"
+                            name="githubUser"
+                            type="text"
+                            placeholder="Usuário Github"
+                        />
+                    </div>
+                    <div>
+                        <Button />
+                    </div>
                 </div>
             </section>
         </main>
